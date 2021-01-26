e8d398e4d6e0ef555355929549cfcffbd5b93688
jlribeiromt <jl.dr@hotmail.com>
2021-01-26 11:28:23 -0400

    Inclusão do projeto CrossCutting - Bus utilizado para envio dos commands e notificações.

---
+++ server/src/ApiRestImportador.Infra.CrossCutting.Bus/InMemoryBus.cs
@@ -0,0 +1,28 @@
+﻿using ApiRestImportador.Domain.Core.Bus;
+using ApiRestImportador.Domain.Core.Commands;
+using ApiRestImportador.Domain.Core.Events;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace ApiRestImportador.Infra.CrossCutting.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public Task RaiseEvent<T>(T @event) where T : Event
+        {
+            return _mediator.Publish(@event);
+        }
+    }
+}
